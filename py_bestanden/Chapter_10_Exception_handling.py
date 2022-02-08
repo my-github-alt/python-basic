1c7b761b20023bd8ac533794806f654a9843e768
my-github-alt <loocat.mhabhatt+github@gmail.com>
2022-02-08 22:47:15 +0100

    uitleg try-except-finally :inbox_tray:

--- py_bestanden/Chapter_10_Exception_handling.py
+++ py_bestanden/Chapter_10_Exception_handling.py
@@ -2,20 +2,26 @@
 # coding: utf-8
 
 # # Exception handling
+# 
+# `try`  
+# `except`  
+# `finally`  
 
-# `try`
-# `except`
-# `finally`
-
-# Mensen maken fouten en programmas worden door mensen geschreven.
-
-# Fouten in een programma kan je opvangen met `try` `except`.
-# In de `try` statement zet je een stuk code dat fout kan gaan.
-# Het blokje code onder de `except` statement vangt de fout op.
-
-# Normaal stopt het programma bij een error, maar als de error is opgevangen gaat het door.
+# Mensen maken fouten en programmas worden door mensen geschreven.  
+# 
+# Fouten in een programma kan je opvangen met `try` `except`.  
+# De code onder het `try` statement mag een error creëren.  
+# Het blokje code onder de `except` statement vangt de gemaakte error op.  
+# 
+# Normaal stopt het programma bij een error, maar als de error is opgevangen gaat het door.  
 
 # ## try except
+# 
+# Exception is een _catch-all_, het is de basis van alle Error Exceptions.  
+# Als de `assert` statement een `False` expressie ontvangt zal `assert` doormiddel van `raise` een AssertionError geven.  
+# Exception zal de AssertionError opvangen.  
+# De error-message die opgegeven is bij `assert` is wordt door `except` aan de `err_msg` variabele gegeven.  
+
 
 try:
     # `raise` express een AssertionError
@@ -28,25 +34,36 @@ print('na de try-except')
 
 
 # ### Multiple excepts
+# Een stuk code kan meerdere soorten errors geven.  
+# Deze kan worden opgevangen door meerdere `except` statements.  
+# De `except` statement kan ook meerdere soorten errors opvangen.  
 
 for num in (3, 2, 1, 0):
     try:
-        # een 
         uitkomst = 10 / num
         print(f'10 gedeeld door {num} is: {uitkomst}')
+        
+    # delen door 0 wordt opgevangen
     except ZeroDivisionError:
         print(f'10 kan niet gedeld worden door 0')
-    except Exception as err_msg:
-        print(f'log: Er is een onbekende fout opgetreden: {err_msg}')
+    
+    # meerdere errors wordt door deze `except` opgevangen
+    except (AssertionError, Exception) as err_msg:
+        print(f'log: Er is een onbekende fout opgetreden:\n\t{err_msg}')
         print('stop het programma')
         exit(1)
 
 
 # ## finally
+# Na de `try` en `except` kan er ook een `finally` statement geschreven worden.  
+# Dit is optioneel maar al het code in het `finally` blok wordt altijd als laatste uitgevoerd.  
+# Het wordt uitgevoerd waneer er een error ontstaat, of de code geen error geeft.  
+# De code in het `finally` blok wordt vaak gebruikt om een actie netjes af te sluiten.  
+# In het volgende voorbeeld wordt er door `finally` een open file altijd netjes gesloten:  
 
 print('open een nieuw bestand')
 # mode van open is 'w' en kan dus alleen str schrijven.
-# 'wb' is de mode die bytes kan schrijven.
+# 'wb' is de mode die bytes kan schrijven. 
 new_file = open('nieuw_bestand.txt', 'w')
 
 try:
@@ -60,7 +77,8 @@ finally:
 
 
 # ### Oefeningen Exception handling
+# 
+# Maak een try-except met een `assert` statement die een error geeft.  
+# Laat de `assert` falen en vang de error op.  
+# print deze error uit in het `finally` blok van de try-except.   
 
-# Maak een try-except met een `assert` statement die een error geeft.
-# Laat de `assert` falen en vang de error op.
-# print deze error uit in het `finally` blok van de try-except. 
